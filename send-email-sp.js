a387a11a30187a491edd3b356d44e9d0ab8eb1ce
Sasho Petkov <petkovsasho@gmail.com>
2018-12-29 12:45:19 +0200

    API header fix

--- send-email-sp.js
+++ send-email-sp.js
@@ -8,6 +8,12 @@
     const port = process.env.PORT || 5000;
 
     app.use( bodyParser.json() );
+    
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 
     // test get
     app.get('/test', function (request, response) {
