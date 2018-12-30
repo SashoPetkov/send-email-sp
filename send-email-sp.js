4915b6b40359b3e5968e2eec5ca0df359a52a546
Sasho Petkov <petkovsasho@gmail.com>
2018-12-30 10:41:56 +0200

    test 10

--- send-email-sp.js
+++ send-email-sp.js
@@ -36,41 +36,41 @@
     // listen form submiting the form form contact us form
     app.post('/api/send-email-sp', function (request, response) {
         
-        // const token_string = Buffer.from(request.body.token, 'base64').toString('Base64');
+        const token_string = Buffer.from(request.body.token, 'base64').toString('Base64');
         
-        // if( token_string.indexOf( 'DQ0NDQ=' > -1 ) ) {
-        //     var transporter = nodemailer.createTransport({
-        //         host: 'smtp.gmail.com',
-        //         port: 587,
-        //         secure: false,
-        //         auth: {
-        //             user: process.env.EMAIL_USERNAME,
-        //             pass: process.env.EMAIL_PASSWORD
-        //         },
-        //     });
+        if( token_string.indexOf( 'DQ0NDQ=' > -1 ) ) {
+            var transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 587,
+                secure: false,
+                auth: {
+                    user: process.env.EMAIL_USERNAME,
+                    pass: process.env.EMAIL_PASSWORD
+                },
+            });
 
-        //     var mailOptions = {
-        //         from: `${request.body.name} <${ request.body.email }>`,
-        //         to: 'petkovsasho@gmail.com',
-        //         subject: request.body.subject || 'subject empty',
-        //         text: request.body.message
-        //     };
+            var mailOptions = {
+                from: `${request.body.name} <${ request.body.email }>`,
+                to: 'petkovsasho@gmail.com',
+                subject: request.body.subject || 'subject empty',
+                text: request.body.message
+            };
 
-        //     transporter.sendMail(mailOptions, function(error, info){
-        //         if (error) {
-        //             console.log(error);
-        //             response.send(`Email is not send! It is some problem ${request.body}`);    
-        //         } else {
-        //             console.log('Email sent: ' + info.response);
-        //             response.send('Email is send successfully !');    
-        //         }
-        //     });
-        // }
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                    response.send(`Email is not send! It is some problem ${request.body}`);    
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    response.send('Email is send successfully !');    
+                }
+            });
+        }
 
-        response.send(`
-                            Email is not send! It is some problem 
-                            ${ request.body }; 
-                    `);     
+        // response.send(`
+        //                     Email is not send! It is some problem 
+        //                     ${ request.body }; 
+        //             `);     
         
     });
 
