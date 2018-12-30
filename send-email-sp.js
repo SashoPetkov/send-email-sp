1aa262d505a8d2364558bd6780f6fae39f4f18bb
Sasho Petkov <petkovsasho@gmail.com>
2018-12-30 12:58:40 +0200

    Final version send mail service.

--- send-email-sp.js
+++ send-email-sp.js
@@ -14,7 +14,7 @@
     });
 
     // test GET request
-    app.get('/test', function (request, response) {
+    app.get('/test-get', function (request, response) {
         response.send( 'hello from GET test request!' );
     }); 
     
@@ -26,11 +26,14 @@
     // listen form submiting the form form contact us form
     app.post('/api/send-email-sp', function (request, response) {
         
-        const token_string = Buffer.from(request.body.token, 'base64').toString('Base64');
-        const emailUserName = process.env.EMAIL_USERNAME;
-        const emailPassword = process.env.EMAIL_PASSWORD;
+        const token_received = Buffer.from(request.body.token, 'base64').toString('Base64');
 
-        if( token_string.indexOf( 'DQ0NDQ=' > -1 ) ) {
+        const emailUserName = process.env.EMAIL_USERNAME_FROM;
+        const emailPassword = process.env.EMAIL_PASSWORD_FROM;
+        const emailTo = process.env.EMAIL_TO;
+        const token_service = process.env.TOKEN;
+
+        if( token_received == token_service ) {
             var transporter = nodemailer.createTransport({
                 host: 'smtp.gmail.com',
                 port: 587,
@@ -42,20 +45,19 @@
             });
 
             var mailOptions = {
-                // from: `${request.body.name} <${ request.body.email }>`,
                 from: `Contact Form <${ request.body.email }>`,
-                to: 'petkovsasho@gmail.com',
+                to: emailTo,
                 subject: request.body.subject || 'subject empty',
-                text: `client name: ${request.body.name} ;
-                       client email: ${request.body.email} ;
-                       message: ${request.body.message} ;`
+                text: `client name: ${request.body.name};
+                client email: ${request.body.email};
+                message: ${request.body.message};`
             };
 
             transporter.sendMail(mailOptions, function(error, info){
                 if (error) {
-                    response.send(`Email is not send! It is some problem ${emailUserName}; ${emailPassword}`);    
+                    response.send(`Email is not send! It is some problem !!! ${error}`);    
                 } else {
-                    response.send('Email is send successfully !');    
+                    response.send('Email was send successfully!!!');    
                 }
             });
         } 
